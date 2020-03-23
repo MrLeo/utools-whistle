72c5c88b3836cb9aab55cb9c5cf7ac85ad2753ff
Leo <leo@xuebin.me>
2020-03-23 10:24:06 +0800

    chore: iframe url

--- src/views/whistle/index.vue
+++ src/views/whistle/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div style="width:100%;height:100%;">
-    <iframe src="http://127.0.0.1:8899/#network" frameborder="0" class="frame" id="frame"></iframe>
+    <iframe src="http://127.0.0.1:8899/" frameborder="0" class="frame" id="frame"></iframe>
     <a-affix
       :offsetTop="50"
       :offsetBottom="50"
