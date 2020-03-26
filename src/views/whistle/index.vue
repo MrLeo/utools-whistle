98eade9ac97e5552c606ab801a250ef8776ca090
Leo <leo@xuebin.me>
2020-03-26 17:35:10 +0800

    process.env.VUE_APP_WHISTLE_API

--- src/views/whistle/index.vue
+++ src/views/whistle/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div style="width:100%;height:100%;">
-    <iframe src="http://local.whistlejs.com/" frameborder="0" class="frame" id="frame"></iframe>
+    <iframe :src="process.env.VUE_APP_WHISTLE_API" frameborder="0" class="frame" id="frame"></iframe>
     <a-affix
       :offsetTop="50"
       :offsetBottom="50"
