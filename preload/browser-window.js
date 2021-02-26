feaa2c0eb5d1177e9dd9e51414c6ea7218997e57
xuebin.liu <xuebin.liu@zhaopin.com.cn>
2021-02-26 13:10:46 +0800

    no message

--- preload/browser-window.js
+++ preload/browser-window.js
@@ -1,4 +1,5 @@
 const { remote, shell } = require('electron')
+const path = require('path')
 
 // const currentWin = remote.getCurrentWindow()
 const currentWeb = remote.getCurrentWebContents()
@@ -11,6 +12,25 @@ currentWeb.on('new-window', (event, url, frameName, disposition, options) => {
   event.preventDefault()
 
   if (!options.webContents) {
-    shell.openExternal(url)
+    // shell.openExternal(url)
+    try {
+      let win = new remote.BrowserWindow({
+        width: 1200,
+        height: 800,
+        webPreferences: {
+          webSecurity: false,
+          preload: path.join(__dirname, 'browser-window.js')
+        }
+      })
+
+      // 开发打开BrowserWindow控制台
+      win.webContents.openDevTools()
+      win.loadURL(url)
+    } catch (err) {
+      window.utools.ubrowser
+        .devTools('bottom')
+        .goto(url)
+        .run({ width: 1200, height: 800 })
+    }
   }
 })
--- src/utils/utools.js
+++ src/utils/utools.js
@@ -88,11 +88,14 @@ export function open(url) {
     return window.utools.ubrowser.show().run(opendBrowser.id)
   }
 
-  return window.utools.ubrowser // .devTools('bottom')
-    .goto(url)
-    .css(
-      `#container { min-width: 100% !important; min-height: 100% !important; }
+  return (
+    window.utools.ubrowser
+      // .devTools('bottom')
+      .goto(url)
+      .css(
+        `#container { min-width: 100% !important; min-height: 100% !important; }
       .w-req-data-content.fill.orient-vertical-box{ min-width: 1100px; }`
-    )
-    .run({ width: 1200, height: 800 })
+      )
+      .run({ width: 1200, height: 800 })
+  )
 }
